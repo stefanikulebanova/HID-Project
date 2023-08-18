def88eab7dae9ffcdb89c7bb500f557b1d9eed27
stefanikulebanova <stefani.kulebanova@gmail.com>
2023-08-18 16:59:44 +0200

    Added profile page and posts

--- project_app/forms.py
+++ project_app/forms.py
@@ -1,19 +1,19 @@
 from typing import Any
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-
-from project_app.models import AppUser
+from django.contrib.auth.forms import UserCreationForm
+from .models import AppUser
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=65)
     password = forms.CharField(max_length=65, widget=forms.PasswordInput)
 
+
 class RegisterForm(UserCreationForm):
     class Meta:
         model = AppUser
         fields = ('username', 'email', 'password1', 'password2', 'is_organizer', 'is_artist', 'bio', 'phone_number', 'image' )
+
     def save(self, commit = True) -> Any:
         user = super().save(commit=False)
         user.is_artist = self.cleaned_data.get('is_artist')
@@ -22,5 +22,6 @@ class RegisterForm(UserCreationForm):
         user.phone_number = self.cleaned_data.get('phone_number')
         user.image = self.cleaned_data.get('image')
 
-        if commit: user.save()
+        if commit:
+            user.save()
         return user
--- project_app/models.py
+++ project_app/models.py
@@ -1,7 +1,4 @@
-from django.db import models
-
-# Create your models here.
-
+import os
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -23,6 +20,7 @@ class Event(models.Model):
     date = models.DateTimeField()
     description = models.CharField(max_length=300, default="Description for event. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor... ")
     available_tickets = models.IntegerField()
+    ticket_price = models.IntegerField(default=10)
     artists = models.ManyToManyField(AppUser, related_name="artists")
     organizer = models.ForeignKey(AppUser, related_name="organizer", on_delete=models.CASCADE)
 
@@ -32,7 +30,7 @@ class Ticket(models.Model):
     user_profile = models.ForeignKey(AppUser, on_delete=models.CASCADE)
     purchase_date = models.DateTimeField(auto_now_add=True)
     qr_code = models.ImageField(upload_to="ticket_qr", default="./static/qrcode.png")
-    price = models.IntegerField(default = 10)
+    price = models.IntegerField(default=10)
 
 
 class Application(models.Model):
@@ -44,6 +42,18 @@ class Application(models.Model):
     status = models.CharField(max_length=150, default="Waiting")
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     artist = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    file = models.FileField(upload_to="posts")
+    description = models.TextField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+
+    def extension(self):
+        name, extension = os.path.splitext(self.file.name)
+        return extension
+
 
 
 
